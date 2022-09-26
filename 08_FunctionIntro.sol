ac69daf468d1edfff30426985a82b7c39eac9447
aybkky <75547266+aybkky@users.noreply.github.com>
2022-09-26 22:55:42 +0300

    Create 08_FunctionIntro.sol

---
+++ 08_FunctionIntro.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FunctionIntro {
+    function add(uint x, uint y) external pure returns (uint) {
+        return x + y;
+    }
+
+    function sub(uint x, uint y) external pure returns (uint) {
+        return x - y;
+    }
+}
+
+//external means when we deploy this contract we'll be able to call this function
+//pure means read only
